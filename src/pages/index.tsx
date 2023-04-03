0bef981b168ff47e7b2dff52a926ee0e55fc1529
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2023-04-03 12:38:56 -0700

    state hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { RandomFox } from "@/components/RandomFox";
+import { useState } from "react";
 
 const random = () => Math.floor(Math.random() * 123) + 1;
 
 const Home: NextPage = () => {
+  const [images, setImages] = useState<string[]>([
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+  ]);
+
   return (
     <>
       <Head>
@@ -15,7 +24,11 @@ const Home: NextPage = () => {
       </Head>
       <main>
         <h1 className="text-3xl font-bold underline">Hello World 😎!</h1>
-        <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`} />
+        {images.map((image, index) => (
+          <div key={index} className="p-4">
+            <RandomFox image={image} />
+          </div>
+        ))}
       </main>
       <footer></footer>
     </>

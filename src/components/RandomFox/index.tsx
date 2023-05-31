3f442d94387f01b4092315fa4f15e6e3324fd17f
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2023-05-31 11:58:06 -0700

    observer for lazzi-loading

--- src/components/RandomFox/index.tsx
+++ src/components/RandomFox/index.tsx
@@ -1,5 +1,33 @@
+import { useRef, useEffect, useState } from "react";
+
 export type Props = { image: string };
 
 export const RandomFox = ({ image }: Props): JSX.Element => {
-  return <img src={image} width={320} height="auto" className="rounded" />;
+  const [src, setSrc] = useState("");
+  const ref = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    // new observer
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        // onIntersection
+        if (entry.isIntersecting) {
+          setSrc(image);
+        }
+      });
+    });
+
+    //observe ref
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    //disconect
+    return () => {
+      observer.disconnect();
+    };
+  }, [image]);
+  return (
+    <img ref={ref} src={src} width={320} height="auto" className="rounded" />
+  );
 };
